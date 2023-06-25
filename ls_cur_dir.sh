f4900f77d035d6dabb1159c86cce310ee47d4c24
simonlearnscoding <simon.muscas@gmail.com>
2023-06-25 09:59:03 +0200

    init

---
+++ ls_cur_dir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+file_to_watch="/home/simon/.current_dir"
+
+dir_to_list=$(cat "$file_to_watch")
+exa --icons -a --group-directories-first --icons "$dir_to_list"
+while inotifywait -q -e modify "$file_to_watch"; do
+	dir_to_list=$(cat "$file_to_watch")
+	clear
+	exa --icons -a --group-directories-first --icons "$dir_to_list"
+done
